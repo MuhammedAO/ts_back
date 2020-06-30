efcd39825c511bb9b56d9b14911a38c5be458dbd
Muhammed ogunsanya <mspeaks910@gmail.com>
2020-06-30 18:22:33 +0100

    build

--- server/models/user.js
+++ server/models/user.js
@@ -1,7 +1,7 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
-const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const saltRounds = 10
+const jwt = require('jsonwebtoken')
 
 const userSchema = mongoose.Schema({
     name: {
@@ -36,15 +36,15 @@ const userSchema = mongoose.Schema({
 
 
 userSchema.pre('save', function( next ) {
-    var user = this;
+    var user = this
     
     if(user.isModified('password')){    
 
         bcrypt.genSalt(saltRounds, function(err, salt){
-            if(err) return next(err);
+            if(err) return next(err)
     
             bcrypt.hash(user.password, salt, function(err, hash){
-                if(err) return next(err);
+                if(err) return next(err)
                 user.password = hash 
                 next()
             })
@@ -52,37 +52,37 @@ userSchema.pre('save', function( next ) {
     } else {
         next()
     }
-});
+})
 
 userSchema.methods.comparePassword = function(plainPassword,cb){
     bcrypt.compare(plainPassword, this.password, function(err, isMatch){
-        if (err) return cb(err);
+        if (err) return cb(err)
         cb(null, isMatch)
     })
 }
 
 userSchema.methods.generateToken = function(cb) {
-    var user = this;
+    var user = this
     var token =  jwt.sign(user._id.toHexString(),'secret')
 
-    user.token = token;
+    user.token = token
     user.save(function (err, user){
         if(err) return cb(err)
-        cb(null, user);
+        cb(null, user)
     })
 }
 
 userSchema.statics.findByToken = function (token, cb) {
-    var user = this;
+    var user = this
 
     jwt.verify(token,'secret',function(err, decode){
         user.findOne({"_id":decode, "token":token}, function(err, user){
-            if(err) return cb(err);
-            cb(null, user);
+            if(err) return cb(err)
+            cb(null, user)
         })
     })
 }
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema)
 
 module.exports = { User }
