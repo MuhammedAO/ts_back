aea2cd497c236c0d3e01da036dee2011b549e0f8
Muhammed ogunsanya <mspeaks910@gmail.com>
2020-06-30 17:41:49 +0100

    Heroku build

--- client/src/components/views/DetailVideoPage/Sections/LikeDislikes.js
+++ client/src/components/views/DetailVideoPage/Sections/LikeDislikes.js
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable array-callback-return */
 import React, { useEffect, useState } from 'react'
 import { Tooltip, Icon } from 'antd';
 import Axios from 'axios';
