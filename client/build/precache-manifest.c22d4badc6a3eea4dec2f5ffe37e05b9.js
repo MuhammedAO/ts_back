0d3d2a526b1538671b7bc175aee1c07d853d9748
Muhammed ogunsanya <mspeaks910@gmail.com>
2020-07-01 11:10:28 +0100

    ENOENT

---
+++ client/build/precache-manifest.c22d4badc6a3eea4dec2f5ffe37e05b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58095793d9ad9316e8e1d800d7585378",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ccf1f4bf9cb546e5d004",
+    "url": "/static/css/2.6547dd70.chunk.css"
+  },
+  {
+    "revision": "e0cb96021ea8a74aa799",
+    "url": "/static/css/main.a1ead8a2.chunk.css"
+  },
+  {
+    "revision": "ccf1f4bf9cb546e5d004",
+    "url": "/static/js/2.a37d1887.chunk.js"
+  },
+  {
+    "revision": "e0cb96021ea8a74aa799",
+    "url": "/static/js/main.72e4e722.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
